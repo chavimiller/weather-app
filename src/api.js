ce983237d42b4b49a53acd1d97ad84110341f39b
chavimiller <chaviweisman@gmail.com>
2025-07-30 15:26:06 +0300

    Create HTML outline and add background images to images folder

--- src/api.js
+++ src/api.js
@@ -1,6 +1,19 @@
 const myApiKey = "8MWEEWA77AGXY3REP34Y2PKHW";
 const location = "Jerusalem";
 
+const iconConditions = [
+  { icon: "snow", url: "./images/Snowy.jpg" },
+  { icon: "rain", url: "./images/Rainy.jpg" },
+  { icon: "fog", url: "./images/Foggy.jpg" },
+  { icon: "wind", url: "./images/Windy.jpg" },
+  { icon: "cloudy", url: "./images/Cloudy.jpg" },
+  { icon: "partly-cloudy-day", url: "./images/PartlyCloudyDay.jpg" },
+  { icon: "partly-cloudy-night", url: "./images/PartlyCloudyNight.jpg" },
+  { icon: "clear-day", url: "./images/Sunny.jpg" },
+  { icon: "clear-night", url: "./images/ClearNight.jpg" },
+];
+const coewn = 2 / 1;
+
 function fToC(fahr) {
   const celsius = (fahr - 32) * (5 / 9);
   return Math.round(celsius * 100) / 100;
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
